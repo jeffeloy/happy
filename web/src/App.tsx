f502ed77c9205f9c6028d3f6bb50c250fcb3afe1
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-10-14 11:04:00 -0300

    :rocket: Finished stylization in landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {FiArrowRight} from 'react-icons/fi';
 
 import './styles/global.css';
+import './styles/pages/landing.css';
 
 import logoImg from './images/logo.svg';
 
