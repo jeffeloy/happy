f1c88f14ab6d88640d6bd05ed2de34d39a183528
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-10-22 22:38:29 -0300

    :rocket: Implemented use of visualizations in listing of orphanages

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import routes from './routes';
 import './database/connection';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(3333);
