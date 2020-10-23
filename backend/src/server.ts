54211fd1ba2f0ccea9cd609e4e2928019281b6d7
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-10-22 22:56:58 -0300

    :sparkles: Added function to handle errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import './database/connection';
 import path from 'path';
+import errorHandler from  './errors/handler';
 
 const app = express();
 
@@ -10,5 +12,6 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 
 app.listen(3333);
